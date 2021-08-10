15a855df825fdb6422790326db282920bee6d382
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2021-08-10 10:21:13 +0530

    Add files via upload

---
+++ ass2/Q1.java
@@ -0,0 +1,13 @@
+package ass2;
+import java.util.Scanner;
+
+public class Q1 {
+	public static void main(String args[])
+	{
+		Scanner myInput = new Scanner( System.in );
+		System.out.print("Enter the time slot: ");
+		int a = myInput.nextInt();
+		System.out.println(a);
+	}
+
+}
